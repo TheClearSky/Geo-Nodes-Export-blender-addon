7da52d6487e3c26bebd2718e9b198e15f2a24689
ClearSky <42476637+TheClearSky@users.noreply.github.com>
2022-05-28 19:23:59 +0530

    initial commit

---
+++ Exporttogltfcustom.py
@@ -0,0 +1,41 @@
+import inspect
+import bpy
+
+def spacing(nooflines,noofcharacters=100):
+    for i in range(nooflines):
+        print("-"*noofcharacters);
+        
+def printattr(obj): #prints names of attributes of an object
+    for i in dir(obj):
+        print(i)
+
+def printattr2(obj): #prints name-value pairs of attributes of an object
+    for i in inspect.getmembers(obj):
+        print(i)
+
+def fixgeometrynodes(geometrymodifier):
+    nodes = geometrymodifier.node_group
+    spacing(2)
+    printattr2(nodes)
+    spacing(2)
+    printattr2(nodes.nodes[0])
+        
+
+ctx = bpy.context
+active_obj = ctx.active_object #get active object
+modifiers = active_obj.modifiers #get modifiers list of active object
+
+printattr2(modifiers[0])
+#currently assuming only one geometry nodes modifier(no stack)
+#loop through all modifiers of active object
+for modifier in modifiers:
+    
+    #if it is a geometry node
+    if modifier.type == "NODES":
+        print("got it")
+        fixgeometrynodes(modifier)
+        break
+    else:
+        print("didn't get")
+
+  
